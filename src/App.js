1e6376238049b9574a0f8bda54b1061cccadcda7
Suhaf Azam Butt <suhaf.azam@gmail.com>
2024-09-19 22:25:18 +0200

    adds nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import logo1 from './logo.svg';
+import logo from './airbnb.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <nav className="header-nav">
+          <img src={logo} alt="logo" />
+        </nav>
+        <img src={logo1} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
